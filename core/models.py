ea85f746ce44b4856332ddc16ac4119fbfced67f
MUGABE GIDEON <mugabegideon44@gmail.com>
2026-03-06 23:31:28 +0300

    Django setup complete - CustomUser model added

--- core/models.py
+++ core/models.py
@@ -1,6 +1,15 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 class CustomUser(AbstractUser):
-  pass
+    ROLE_CHOICES =[
+        ('student', 'Student Intern'),
+        ('workplace_supervisor', 'Academic Supervisor'),
+        ('academic_supervisor', 'Academic Supervisor'),
+        ('admin', 'Administrator'),
+    ]
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, default='student')
+    phone= models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return (f"{self.username},({self.role})")
