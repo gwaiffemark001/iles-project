7cc40ddde3d3da5a0eeeac9634946928e798af0c
gwaiffemark <mar666068@gmail.com>
2026-03-09 23:29:22 +0300

    'created custom user model and registered it in admin interface'

--- core/models.py
+++ core/models.py
@@ -3,4 +3,31 @@ from django.db import models
 
 # Create your models here.
 class CustomUser(AbstractUser):
-  pass
+    # Choices for roles 
+    ROLE_CHOICES = [
+        ('student', 'Student Intern'),
+        ('workplace_sup', 'Workplace Supervisor'),
+        ('academic_sup', 'Academic Supervisor'),
+        ('admin', 'Internship Admin'),
+    ]
+    
+    # Fields
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='student' 
+    )
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    profile_picture = models.ImageField(
+        upload_to='profiles/',
+        blank=True,
+        null=True 
+    )
+    bio = models.TextField(
+        max_length=500,
+        blank=True
+    )
+    
+    # Method to display user name and role in admin interface
+    def __str__(self):
+        return f'{self.username} ({self.get_role_display()})'
