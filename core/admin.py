8eedd95662dec345941f560991ea667756797434
gwaiffemark <mar666068@gmail.com>
2026-03-11 12:32:32 +0300

    registering models in admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import CustomUser
+from .import models
 
 # Register your models here.
-admin.site.register(CustomUser)
+admin.site.register(models.CustomUser)
+admin.site.register(models.InternshipPlacement)
