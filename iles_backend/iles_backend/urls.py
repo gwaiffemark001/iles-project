e3dd01efa2a0fea03f2bb995778681e611b4b7ce
MUGABE GIDEON <mugabegideon44@gmail.com>
2026-03-26 14:49:23 +0300

    Added InternshipPlacementListView API endpoint

--- iles_backend/iles_backend/urls.py
+++ iles_backend/iles_backend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from core.views import WeeklyLogListView, WeeklyLogDetailView
+from core.views import WeeklyLogListView, WeeklyLogDetailView, InternshipPlacementListView
 
 urlpatterns =[
     path('admin/',  admin.site.urls),
@@ -13,4 +13,5 @@ urlpatterns =[
     path('api/logs/', WeeklyLogListView.as_view(), name='weekly-log-list'),
     path('api/logs/<int:pk>/', WeeklyLogDetailView.as_view(), name='weekly-log-detail'),
 
+    path('api/placements/', InternshipPlacementListView.as_view(), name='placement-list'),
 ]
