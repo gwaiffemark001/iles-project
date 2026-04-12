5dd06fcd528e932d64629a1a270fbd5f7e1f8b4a
Talituleka-Hope-Mwleu <talitulekah@gmail.com>
2026-04-12 22:41:51 +0300

    feat: add workplace supervisor dashboard placeholder

--- iles-frontend/src/App.jsx
+++ iles-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
+import WorkplaceSupervisorDashboard from "./pages/WorkplaceSupervisor/WorkplaceSupervisorDashboard";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/workplace-supervisor/dashboard" element={<WorkplaceSupervisorDashboard />} />
       </Routes>
     </BrowserRouter>
   );
