174dc8a1106b2efc95442b0fef093603ad268e04
fsena42 <sekirandaemma256@gmail.com>
2026-04-13 20:20:18 +0300

    feature: changes made to frontend and frontconnected to backend

--- iles_frontend/src/ForgotPassword.jsx
+++ iles_frontend/src/ForgotPassword.jsx
@@ -6,18 +6,20 @@ function ForgotPassword() {
     return (
         <>
             <div className='page_1'>
-                <header className='header_1'>
-                    <h1 className="head">RESET PASSWORD</h1>
-                    <img className="logo" src="/ILES-Logo.png" alt="ILES logo" />
-                </header>
-                <section className="logins">
-                    <input type="email" placeholder="Enter your email"></input>
-                    <button className="login-btn">Send Reset Link</button>
-                </section>
-                <section className='failed_login'>
-                    <p className="signup"><Link to="/">Back to Login</Link></p>
-                    <p className="signup"><Link to="/signup">Sign up</Link></p>
-                </section>
+                <div>
+                    <header className='header_1'>
+                        <h1 className="head">RESET PASSWORD</h1>
+                        <img className="logo" src="/ILES-Logo.png" alt="ILES logo" />
+                    </header>
+                    <form className="logins">
+                        <input type="email" placeholder="Enter your email"></input>
+                        <button className="login-btn">Send Reset Link</button>
+                    </form>
+                    <section className='failed_login'>
+                        <p className="signup"><Link to="/">Back to Login</Link></p>
+                        <p className="signup"><Link to="/signup">Sign up</Link></p>
+                    </section>
+                </div>
             </div>
         </>
     );
