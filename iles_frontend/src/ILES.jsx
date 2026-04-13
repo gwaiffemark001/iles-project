bcba8e41686d2a09b4666e932ea6bc540b5c0b9d
patrickogesa <patrickogesagp@gmail.com>
2026-04-13 21:02:46 +0300

    #Started on the styling of the student dashboard

--- iles_frontend/src/ILES.jsx
+++ iles_frontend/src/ILES.jsx
@@ -2,15 +2,20 @@ import { Routes, Route } from 'react-router-dom';
 import Firstpage from './Firstpage';
 import Signup from './Signup';
 import ForgotPassword from './ForgotPassword';
-import InternshipDashboard from './StudentDash.jsx';
+import StudentDash from './StudentDash';
+import InternshipDashboard from './StudentDash';
 
 function ILES() {
     return (
+        <> 
         <Routes>
             <Route path="/" element={<Firstpage />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
+        <InternshipDashboard/>
+        
+        </>
     );
 }
 
