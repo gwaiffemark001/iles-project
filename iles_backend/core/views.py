4f1104cc257f886ec69f3bc3e052e1f5828a7968
MUGABE GIDEON <mugabegideon44@gmail.com>
2026-04-13 20:26:44 +0300

    Add SupervisorApproveView - supervisor can approve reviewed logs

--- iles_backend/core/views.py
+++ iles_backend/core/views.py
@@ -210,3 +210,29 @@ class SupervisorReviewView(APIView):
 
         serializer = WeeklyLogSerializer(log)
         return Response(serializer.data)
+
+class SupervisorApproveView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, pk):
+        if request.user.role not in ['workplace_supervisor', 'academic_supervisor', 'admin']:
+            return Response(
+                {'error': 'Only supervisors can approve logs'},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        try:
+            log = WeeklyLog.objects.get(pk=pk)
+        except WeeklyLog.DoesNotExist:
+            return Response({'error': 'Log not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        if log.status != 'reviewed':
+            return Response(
+                {'error': 'Only reviewed logs can be approved'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        log.status = 'approved'
+        log.save()
+
+        serializer = WeeklyLogSerializer(log)
+        return Response(serializer.data)
