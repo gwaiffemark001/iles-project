62a6113963e9d2aa91cc53f4c0d199c5523b9b2c
Talituleka-Hope-Mwleu <talitulekah@gmail.com>
2026-04-14 15:47:15 +0300

    chore: relocate work from iles-frontend to iles_frontend

--- iles_frontend/src/main.jsx
+++ iles_frontend/src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
 import './index.css'
-import ILES from './ILES.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <ILES />
-  </BrowserRouter>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
-
-
