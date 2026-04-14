90d76e08423bb0ef1d1233e48c2fce2c62805659
Talituleka-Hope-Mwleu <talitulekah@gmail.com>
2026-04-14 10:16:19 +0300

    feat: add academic supervisor dashboard route to App.jsx

--- iles-frontend/src/App.jsx
+++ iles-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import WorkplaceSupervisorDashboard from "./pages/WorkplaceSupervisor/WorkplaceSupervisorDashboard";
+import AcademicSupervisorDashboard from "./pages/AcademicSupervisor/AcademicSupervisorDashboard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/workplace-supervisor/dashboard" element={<WorkplaceSupervisorDashboard />} />
+        <Route path="/academic-supervisor/dashboard" element={<AcademicSupervisorDashboard />} />
       </Routes>
     </BrowserRouter>
   );
