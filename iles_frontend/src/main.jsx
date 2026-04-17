f4700a2d43130f8283ba11ecd94787278e7adc28
gwaiffe mark <mar666068@gmail.com>
2026-04-17 12:22:33 +0300

    "modified iles_frontend/src/main.jsx"

--- iles_frontend/src/main.jsx
+++ iles_frontend/src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import App from './App';
+import ILES from './ILES';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ILES />
   </React.StrictMode>
 );
