583ac9ecbc4b8b56e5481b2e29e70033855ac2f5
Talituleka-Hope-Mwleu <talitulekah@gmail.com>
2026-04-18 00:00:50 +0300

    feat: Add academic supervisor dashboard files to ILES.jsx

--- iles_frontend/src/ILES.jsx
+++ iles_frontend/src/ILES.jsx
@@ -1,22 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Firstpage from './Firstpage';
 import Signup from './Signup';
 import ForgotPassword from './ForgotPassword';
 import StudentDash from './StudentDash';
+import WorkplaceSupervisorDashboard from "./pages/WorkplaceSupervisor/WorkplaceSupervisorDashboard";
+import AcademicSupervisorDashboard from "./pages/AcademicSupervisor/AcademicSupervisorDashboard";
 
 
 function ILES() {
     return (
-        <> 
+        <BrowserRouter> 
         <Routes>
             <Route path="/" element={<Firstpage />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path='/studentdashboard' element={ <studentDashboard />}/>
+            <Route path="/workplace-supervisor/dashboard" element={<WorkplaceSupervisorDashboard />} />
+            <Route path="/academic-supervisor/dashboard" element={<AcademicSupervisorDashboard />} />
         </Routes>
        
         
-        </>
+        </BrowserRouter>
     );
 }
 
