0865b66111feacd3581d6d7fdccdf7b3af462584
fsena42 <sekirandaemma256@gmail.com>
2026-04-18 10:17:01 +0300

    changes made on front end

--- iles_frontend/src/ForgotPassword.jsx
+++ iles_frontend/src/ForgotPassword.jsx
@@ -3,19 +3,21 @@ import './Firstpage.css';
 import './ILES.css';
 
 function ForgotPassword() {
+
+
     return (
         <>
-            <div className='page_1'>
-                <div>
+            <div className='page_1' style={{ height: "100vh" } }>
+                <div className='centre_logins'>
                     <header className='header_1'>
                         <h1 className="head">RESET PASSWORD</h1>
                         <img className="logo" src="/ILES-Logo.png" alt="ILES logo" />
                     </header>
                     <form className="logins">
                         <input type="email" placeholder="Enter your email"></input>
-                        <button className="login-btn">Send Reset Link</button>
+                        <button type='submit' className="login-btn">Send Reset Link</button>
                     </form>
-                    <section className='failed_login'>
+                    <section className='failed_login' style={{width: "400px"}}>
                         <p className="signup"><Link to="/">Back to Login</Link></p>
                         <p className="signup"><Link to="/signup">Sign up</Link></p>
                     </section>
