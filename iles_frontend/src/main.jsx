85abb477882130f12ef60adb94f7b18e41cf8239
fsena42 <sekirandaemma256@gmail.com>
2026-04-18 10:24:18 +0300

    fix: failed to run error

--- iles_frontend/src/main.jsx
+++ iles_frontend/src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -12,16 +11,3 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </StrictMode>
 )
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-
-import ILES from './ILES';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ILES />
-  </React.StrictMode>
-);
->>>>>>> f2cc4a6c18ababc03fd7be5297fac41245ad1e24
