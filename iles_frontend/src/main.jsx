51adc7a0095b164ce10eabba6bd92df974d844bf
Talituleka-Hope-Mwleu <talitulekah@gmail.com>
2026-04-26 16:57:03 +0300

    chore: remove App.jsx and use ILES.jsx as main router

--- iles_frontend/src/main.jsx
+++ iles_frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import ILES from './ILES'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ILES />
   </StrictMode>,
 )
