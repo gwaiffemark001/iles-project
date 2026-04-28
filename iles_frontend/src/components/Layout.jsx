dd9a4dba1a9d4429b8edefd0351e6e90f746a40d
Talituleka-Hope-Mwleu <talitulekah@gmail.com>
2026-04-28 16:59:44 +0300

    feat: updated routing and readme

---
+++ iles_frontend/src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout-container">
+      {/* You can add a Navbar or Sidebar here later */}
+      <main>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
