8e3997ea0afb3e8115af0fba8f3452a0da9ac17c
fsena42 <sekirandaemma256@gmail.com>
2026-05-19 13:36:16 +0300

    feat: Add custom hook for async fetch loading and error state

--- iles_frontend/src/hooks/useFetch.js
+++ iles_frontend/src/hooks/useFetch.js
@@ -3,6 +3,14 @@ import { useCallback, useState } from "react";
 function useFetch(fetchFn) {
     const [loading, setLoading] = useState(false)
     const [error, seterror] = useState(null)
+
+    const execute = useCallback(
+        async (...args) => {
+            setLoading(true)
+            seterror(null)
+        }, 
+        [fetchFn],
+    )
 }
 
 export default useFetch
