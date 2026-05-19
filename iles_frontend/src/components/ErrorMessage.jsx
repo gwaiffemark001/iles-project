5997d75bab2b2b7c9154a179a3efbf37405ef4cc
fsena42 <sekirandaemma256@gmail.com>
2026-05-19 12:01:09 +0300

    docs: add alternative code to ErrorMessage.jsx as comment

--- iles_frontend/src/components/ErrorMessage.jsx
+++ iles_frontend/src/components/ErrorMessage.jsx
@@ -5,4 +5,13 @@ export default function ErrorMessage({ message }) {
       {message}
     </div>
   )
+  /*
+    if (message === null) return null
+  return (
+    <div className="iles-error-message" role="alert" aria-live="assertive">
+      {message}
+    </div>
+  )
+ */
+
 }
