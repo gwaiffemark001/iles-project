6f9eefeb5a46bb5f4a377cd71a41f01e5c0279c0
MUGABE GIDEON <mugabegideon44@gmail.com>
2026-05-29 20:26:44 +0300

    Added userprofile sec

--- iles_backend/core/apps.py
+++ iles_backend/core/apps.py
@@ -6,27 +6,3 @@ class CoreConfig(AppConfig):
 
     def ready(self):
         from . import signals  # noqa: F401
-"""
-a
-b
-c
-d
-e
-f
-g
-h
-i
-globalj
-k
-t
-f
-e
-y
-j
-u
-kg
-r
-t
-h
-h
-"""        
