e34670f19bef38d0869e714f7b17d036a70344e5
MUGABE GIDEON <mugabegideon44@gmail.com>
2026-05-29 19:55:42 +0300

    fix(admin): move status banner into content area

--- iles_frontend/src/hooks/useInterval.js
+++ iles_frontend/src/hooks/useInterval.js
@@ -19,3 +19,25 @@ export default function useInterval(callback, delay) {
     return () => clearInterval(intervalId)
   }, [delay])
 }
+
+import { useEffect, useRef } from 'react'
+
+export default function useInterval(callback, delay) {
+  const savedCallback = useRef(callback)
+
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  useEffect(() => {
+    if (delay === null || delay === undefined) {
+      return undefined
+    }
+
+    const intervalId = setInterval(() => {
+      savedCallback.current()
+    }, delay)
+
+    return () => clearInterval(intervalId)
+  }, [delay])
+}
