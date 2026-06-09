2ec5f883fcd25d6522bc7e6913772dae405cdad8
gwaiffe mark <mar666068@gmail.com>
2026-06-10 01:01:54 +0300

    "fix: remove proxy and add production env"

--- iles_frontend/vite.config.js
+++ iles_frontend/vite.config.js
@@ -1,29 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),   // @ points to src/
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
-
-
-
-
-
-
-
