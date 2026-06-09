e63adf6160097253434a4115b97fb4435c4d2e45
gwaiffe mark <mar666068@gmail.com>
2026-06-10 02:36:34 +0300

    "MODIFIED settings.py"

--- iles_backend/iles_backend/settings.py
+++ iles_backend/iles_backend/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key-change-in-production')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost','127.0.0.1').split(',')
 #ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'core.CustomUser'
 
